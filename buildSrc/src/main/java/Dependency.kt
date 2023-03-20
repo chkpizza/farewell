82f7f85e0ceff46c7e8213294ecaba15834fd4d5
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2023-03-20 16:22:05 +0900

    Initial commit

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,46 @@
+object AndroidX {
+    const val CORE = "androidx.core:core-ktx:1.7.0"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.6.1"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1"
+    const val LIVE_DATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.5.1"
+    const val NAVIGATION_RUNTIME = "androidx.navigation:navigation-runtime-ktx:2.5.3"
+    const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:2.5.3"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:2.5.3"
+    const val SWIPE_REFRESH_LAYOUT = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
+    const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.3.0"
+    const val SPLASH_SCREEN = "androidx.core:core-splashscreen:1.0.0-beta02"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.8.0"
+    const val GOOGLE_PLAY_SERVICE = "com.google.android.gms:play-services-auth:20.4.1"
+    const val DAGGER = "com.google.dagger:dagger:2.44"
+    const val DAGGER_COMPILER = "com.google.dagger:dagger-compiler:2.44"
+}
+
+object Kotlin {
+    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
+}
+
+object Firebase {
+    const val FIREBASE_BOM = "com.google.firebase:firebase-bom:31.2.3"
+    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics-ktx"
+    const val FIREBASE_AUTH = "com.google.firebase:firebase-auth-ktx"
+    const val FIREBASE_REALTIME_DATABASE = "com.google.firebase:firebase-database-ktx"
+    const val FIREBASE_STORAGE = "com.google.firebase:firebase-storage-ktx"
+}
+
+object AndroidTest {
+    const val JUNIT4: String = "junit:junit:4.13.2"
+    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.5"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.5.1"
+}
+
+object ThirdParty {
+    const val GLIDE = "com.github.bumptech.glide:glide:4.14.2"
+    const val GLIDE_COMPILER = "com.github.bumptech.glide:compiler:4.14.2"
+    const val CIRCULAR_IMAGE_VIEW = "com.mikhaellopez:circularimageview:4.3.1"
+    const val DOTS_INDICATOR = "com.tbuonomo:dotsindicator:4.3"
+    const val CARD_STACK_VIEW = "com.github.yuyakaido:cardstackview:2.3.4"
+}
