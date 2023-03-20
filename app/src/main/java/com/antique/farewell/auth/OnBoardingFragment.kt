04eede8e5bef7fd41e9d0cd4426851c4981c67fb
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2023-03-20 17:35:28 +0900

    decorate AuthFragment layout

--- app/src/main/java/com/antique/farewell/auth/OnBoardingFragment.kt
+++ app/src/main/java/com/antique/farewell/auth/OnBoardingFragment.kt
@@ -1,20 +1,60 @@
 package com.antique.farewell.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.updatePadding
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.antique.farewell.MainActivity
 import com.antique.farewell.R
+import com.antique.farewell.databinding.FragmentOnBoardingBinding
 
 
 class OnBoardingFragment : Fragment() {
+    private var _binding: FragmentOnBoardingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_on_boarding, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+    }
+
+    private fun initialize() {
+        setUpInsets()
+        setUpViewListener()
+    }
+
+    private fun setUpInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            view.updatePadding(
+                top = insets.systemWindowInsets.top,
+                bottom = insets.systemWindowInsets.bottom
+            )
+            insets
+        }
+    }
+
+    private fun setUpViewListener() {
+        binding.signInButtonView.setOnClickListener {
+            findNavController().navigate(R.id.action_onBoardingFragment_to_authFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
